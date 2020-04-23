c7689527c5d3d2d569660280ac69194a6ba0bb98
IsaacMaciel <isaac_maciel@hotmail.com>
2020-04-23 11:50:15 -0300

    correção do banco

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const {Pool} = require('pg');
 
 module.exports = new Pool({
     user:"postgres",
-    password:"@mdk3t50",
+    password:"@postgres",
     host:"localhost",
     port:5432,
     database:"schoolmanager"
